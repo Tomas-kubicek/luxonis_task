c51d3b19d84079b8da3175f0d31ceeb4318c96ec
Tomáš Kubíček <tomas.kubicek69@gmail.cz>
2023-11-09 21:27:53 +0100

    quick fixes

--- web/app.py
+++ web/app.py
@@ -15,9 +15,15 @@ def index():
     password = os.environ["POSTGRES_PASSWORD"]
     database = os.environ["POSTGRES_DB"]
 
-    # Create/Connect to database
     with psycopg2.connect(host=hostname, user=username, password=password, dbname=database) as connection:
         with connection.cursor() as cur:
+            # Create quotes table if none exists, these (this one and the one in the scraper container) are certainly not the best solution.
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS sreality(
+                id serial PRIMARY KEY, 
+                title text,
+                image_url text
+            )""")
             cur.execute("select title,image_url from sreality")
             items = "\n".join((item_format.format(
                 title=title, image_url=image_url, i=i+1) for i, (title, image_url) in enumerate(cur.fetchall())))
